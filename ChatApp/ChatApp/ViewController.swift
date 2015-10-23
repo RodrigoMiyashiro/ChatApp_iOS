b3037d98f434547486f71c69f91c0ee6c82766c0
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2015-10-23 18:10:56 -0200

    Starting the project.

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Rodrigo A E Miyashiro on 10/23/15.
+//  Copyright © 2015 Rodrigo A E Miyashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
