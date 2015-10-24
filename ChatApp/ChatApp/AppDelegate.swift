98bc5cb5f04fe4df31ce26a48b2167efafd1a559
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2015-10-24 00:39:40 -0200

    Parse framework added.

--- ChatApp/ChatApp/AppDelegate.swift
+++ ChatApp/ChatApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Bolts
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("QtTUrIct1rpX8DwpvUJzAgiIZ0hoMYcfz27tScfv",
+            clientKey: "CGU7aASnaUwyQa9n5bef4PchIilHtuoLi1LKQhAj")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        
         return true
     }
 
